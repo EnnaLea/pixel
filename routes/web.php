c113b4c958548ab83b10df90f4a6cb363eb4262b
EnnaLea <soleariaeve@gmail.com>
2024-06-07 18:01:23 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,22 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class,'index']);
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisteredUserController::class,'create']);
+    Route::post('/register', [RegisteredUserController::class,'store']);
+    
+    Route::get('login', [SessionController::class,'create']);
+    Route::get('login', [SessionController::class,'store']);
+    Route::get('login', [SessionController::class,'destroy']);
+
+});
+
+
+
+Route::delete('/logout', [SessionController::class,'destroy'])->middleware('auth');
